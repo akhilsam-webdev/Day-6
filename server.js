65466dc1b6e6382877477b9b4f2d31e7e8538d91
Akhil sambasivan <akhilsam7722@gmail.com>
2026-04-02 18:49:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const app = require("./src/app");
+const mongoose = require("mongoose");
+
+function connectToDb() {
+  mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("connected to Database");
+  });
+}
+connectToDb();
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
